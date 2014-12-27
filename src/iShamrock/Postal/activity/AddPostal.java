6b6cee40df57f3ca6d98f08e65ae522ed5977828
geeeeeeeeek <810421910@qq.com>
2014-12-27 16:41:49 +0800

    working on Add Postal

--- src/iShamrock/Postal/activity/AddPostal.java
+++ src/iShamrock/Postal/activity/AddPostal.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import iShamrock.Postal.R;
 
@@ -25,11 +26,26 @@ public class AddPostal extends Activity {
     private final String IMAGE_UNSPECIFIED = "image/*";
     private String imageDir;
     private ImageView imageView;
+    private int width, height;
+    private com.gc.materialdesign.views.ButtonRectangle btnImage;
+    private com.gc.materialdesign.views.ButtonRectangle btnTake;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.addpostal);
+
+
+        imageView = (ImageView) findViewById(R.id.add_postal_imageView);
+
+        int screenWidth = this.getWindowManager().getDefaultDisplay().getWidth();
+        float scale = this.getResources().getDisplayMetrics().densityDpi;
+        width = (int) (screenWidth / scale * 160);
+        height = width / 4 * 3;
+
+        ViewGroup.LayoutParams lyp = imageView.getLayoutParams();
+        lyp.height = screenWidth / 4 * 3;
+        imageView.setLayoutParams(lyp);
 
         /*WebView postalContainer = (WebView) findViewById(R.id.postal_container);
 
@@ -45,7 +61,8 @@ public class AddPostal extends Activity {
 
         postalContainer.loadUrl("file:///android_asset/web/content.html");*/
 
-        com.gc.materialdesign.views.ButtonRectangle btnImage = (com.gc.materialdesign.views.ButtonRectangle) findViewById(R.id.btn_img);
+
+        btnImage = (com.gc.materialdesign.views.ButtonRectangle) findViewById(R.id.btn_img);
         btnImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -56,9 +73,8 @@ public class AddPostal extends Activity {
             }
         });
 
-        imageView = (ImageView) findViewById(R.id.imageView2);
 
-        com.gc.materialdesign.views.ButtonRectangle btnTake = (com.gc.materialdesign.views.ButtonRectangle) findViewById(R.id.btn_take_photo);
+        btnTake = (com.gc.materialdesign.views.ButtonRectangle) findViewById(R.id.btn_take_photo);
         btnTake.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -68,43 +84,38 @@ public class AddPostal extends Activity {
                 startActivityForResult(intent, TAKE_PHOTO);
             }
         });
+
+//        btnTake.setPadding(0, 360, 50, 0);
+
     }
 
     private void photoZoom(Uri uri) {
         Intent intent = new Intent("com.android.camera.action.CROP");
         intent.setDataAndType(uri, IMAGE_UNSPECIFIED);
         intent.putExtra("crop", "true");
-        intent.putExtra("aspectX", "1");
-        intent.putExtra("aspectY", "1");
+        intent.putExtra("aspectX", 4);
+        intent.putExtra("aspectY", 3);
 
-        intent.putExtra("outputX", 250);
-        intent.putExtra("outputY", 250);
+        intent.putExtra("outputX", width);
+        intent.putExtra("outputY", height);
         intent.putExtra("return-data", true);
         startActivityForResult(intent, PHOTO_RESULT);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == PHOTO_ZOOM) {
-            photoZoom(data.getData());
-            return;
-        }
-        if (requestCode == TAKE_PHOTO) {
-            File picture = new File(Environment.getExternalStorageDirectory() + "/" + imageDir);
-            photoZoom(Uri.fromFile(picture));
-            return;
-        }
+        if (requestCode == PHOTO_ZOOM) photoZoom(data.getData());
+        if (requestCode == TAKE_PHOTO)
+            photoZoom(Uri.fromFile(new File(Environment.getExternalStorageDirectory() + "/" + imageDir)));
         if (requestCode == PHOTO_RESULT) {
-            Bundle extras = data.getExtras();
+            Bundle extras = null;
+            if (data != null) extras = data.getExtras();
             if (extras != null) {
                 Bitmap photo = extras.getParcelable("data");
-                ByteArrayOutputStream stream = new ByteArrayOutputStream();
-                photo.compress(Bitmap.CompressFormat.JPEG, 75, stream);
+                photo.compress(Bitmap.CompressFormat.JPEG, 100, new ByteArrayOutputStream());
                 imageView.setImageBitmap(photo);
             }
-
         }
-
-//        super.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
