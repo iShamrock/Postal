0e4682c074b78a7b61d981327110724546d2ad50
geeeeeeeeek <810421910@qq.com>
2015-02-16 12:00:01 +0800

    minor change

---
+++ src/iShamrock/Postal/activity/publishers/TextPublisher.java
@@ -0,0 +1,14 @@
+package iShamrock.Postal.activity.publishers;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Tong on 02.15.
+ */
+public class TextPublisher extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
